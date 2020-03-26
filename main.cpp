cea59461b82d7de04dd33367b1a3bc02cc31c568
imnetcat <guskov.danil@gmail.com>
2020-03-26 14:37:17 +0200

    вынес тесты в отдельный файл

--- main.cpp
+++ main.cpp
@@ -1,36 +1,7 @@
 #include "email.h"
-#include <iostream>
+#include "test.h"
 
 int main()
 {
-	EMAIL mail;
-
-	mail.useGmail();
-	mail.SetAuth("my.gmail@gmail.com", "my_password");
-	mail.SetSecurity(USE_TLS);
-
-	mail.SetSenderName("User");
-	mail.SetSenderMail("my.gmail@gmail.com");
-	mail.SetReplyTo("my.gmail@gmail.com");
-	mail.SetSubject("Test");
-	mail.AddRecipient("friend.mail@gmail.com");
-	mail.SetXPriority(XPRIORITY_NORMAL);
-	mail.SetXMailer("The Bat! (v3.02) Professional");
-	mail.AddMsgLine("Hello,");
-	mail.AddMsgLine("");
-	mail.AddMsgLine("...");
-	mail.AddMsgLine("How are you today?");
-	mail.AddMsgLine("");
-	mail.AddMsgLine("Regards");
-	mail.ModMsgLine(5, "regards");
-	mail.DelMsgLine(2);
-	mail.AddMsgLine("User");
-
-	//mail.AddAttachment("../test1.jpg");
-	//mail.AddAttachment("c:\\test2.exe");
-	//mail.AddAttachment("c:\\test3.txt");
-	mail.send();
-
-	std::cout << "Mail was send successfully.\n";
-	return 0;
+	return test();
 }
