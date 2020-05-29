88d6fd58bdccf82da92c0ba00308e60e668ed9d6
imnetcat <guskov.danil@gmail.com>
2020-05-29 17:29:11 +0300

    отделил шифрование с помощью openssl от протокола smtp

--- esmtps.h
+++ esmtps.h
@@ -3,21 +3,14 @@
 #define _SMTPS_H_
 
 #include "esmtp.h"
+#include "OpenSSL.h"
 
-#include <openssl/ssl.h>
-#include <openssl/err.h>
-
-#pragma comment (lib, "crypt32")
-#pragma comment (lib, "libcrypto64MTd.lib")
-#pragma comment (lib, "libssl64MTd.lib")
-
-class ESMTPS : public ESMTP
+class ESMTPS : public ESMTP, private OpenSSL
 {
 public:
 	ESMTPS();
 	~ESMTPS();
 
-	// TLS/SSL extension
 	enum SMTP_SECURITY_TYPE
 	{
 		NO_SECURITY,
@@ -31,8 +24,6 @@ public:
 	RETCODE SendData(int timeout);
 	RETCODE ReceiveData(int timeout);
 	RETCODE Send(MAIL m, SMTP_SECURITY_TYPE sec);
-	RETCODE ReceiveData_SSL(int send_timeout);
-	RETCODE SendData_SSL(int send_timeout);
 protected:
 	static COMMAND STARTTLS = 9;
 	RETCODE Command(COMMAND command);
@@ -40,9 +31,6 @@ protected:
 
 	void CleanupOpenSSL();
 	RETCODE OpenSSLConnect();
-	RETCODE InitOpenSSL();
-	SSL_CTX*      ctx;
-	SSL*          ssl;
 };
 
 #endif
