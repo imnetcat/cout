fc7dc11219ae26429991d5e2cf7cfa1d3a496147
imnetcat <guskov.danil@gmail.com>
2020-06-04 15:31:12 +0300

    отделил определения от обьявления

---
+++ raw.cpp
@@ -0,0 +1,35 @@
+#include "raw.h"
+
+Raw::Raw()
+{
+	m_sLocalHostName = GetLocalName();
+}
+
+void Raw::Connect()
+{
+	if (hSocket == INVALID_SOCKET)
+	{
+		if (SocksConnect(server.name, server.port))
+		{
+			DEBUG_LOG(1, "Ошибка при соеденении");
+			server.isConnected = false;
+			Disconnect();
+			throw FAIL(WSA_INVALID_SOCKET);
+		}
+	}
+}
+
+void Raw::Disconnect()
+{
+	Socket::Disconnect();
+}
+
+void Raw::Send()
+{
+	Socket::Input(SendBuf.c_str(), SendBuf.size());
+	SendBuf.clear();
+}
+void Raw::Receive()
+{
+	RecvBuf += Socket::Output();
+}
--- raw.h
+++ raw.h
@@ -7,40 +7,11 @@
 class Raw : public Protocol, protected Socket
 {
 public:
-	Raw()
-	{
-		m_sLocalHostName = GetLocalName();
-	}
-
-	void Connect() override
-	{
-		if (hSocket == INVALID_SOCKET)
-		{
-			if (SocksConnect(server.name, server.port))
-			{
-				DEBUG_LOG(1, "Ошибка при соеденении");
-				server.isConnected = false;
-				Disconnect();
-				throw FAIL(WSA_INVALID_SOCKET);
-			}
-		}
-	}
-
-	void Disconnect() override
-	{
-		Socket::Disconnect();
-	}
-
-	void Send() override
-	{
-		Socket::Input(SendBuf.c_str(), SendBuf.size());
-		SendBuf.clear();
-	}
-	void Receive() override
-	{
-		RecvBuf += Socket::Output();
-	}
-
+	Raw();
+	void Connect() override;
+	void Disconnect() override;
+	void Send() override;
+	void Receive() override;
 protected:
 	struct SERVER
 	{
