9c6fc0020580b884f8f11e3b2f3908715959d93b
imnetcat <guskov.danil@gmail.com>
2020-06-06 19:48:04 +0300

    изменил на namespace

--- auth.h
+++ auth.h
@@ -2,19 +2,15 @@
 #ifndef _AUTH_H_
 #define _AUTH_H_
 #include "core.h"
-
 #include <string>
 #include <sstream>
 #include <ctime>
-using string = std::string;
-
-class Auth
+namespace Auth
 {
-public:
-	static string Plain(const string& login, const string& pass);
-	static string Login(const string& credentials);
-	static string CramMD5(const string& encoded_challenge, const string& login, const string& pass);
-	static string DigestMD5(const string& encoded_challenge, const string& charset, const string& addr, const string& login, const string& pass);
+	using string = std::string;
+	string Plain(const string& login, const string& pass);
+	string Login(const string& credentials);
+	string CramMD5(const string& encoded_challenge, const string& login, const string& pass);
+	string DigestMD5(const string& encoded_challenge, const string& charset, const string& addr, const string& login, const string& pass);
 };
-
 #endif
